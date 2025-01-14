ed1dd5ab3e5251b5ac585d1d06d70b9e1329a32a
headhunterakali0 <headhunterakali0@gmail.com>
2025-01-14 07:26:21 +0000

    Add Noise and configure svgr

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
+'use client'
+
 import "@/styles/globals.css";
+import gsap from "gsap";
+import Tempus from "tempus";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import dynamic from "next/dynamic";
+
+if (typeof window !== 'undefined') {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.ticker.lagSmoothing(0);
+  gsap.ticker.remove(gsap.updateRoot);
+  Tempus.add((time) => {
+    gsap.updateRoot(time / 1000)
+  }, 0)
+}
+
+const Noise = dynamic(() => import('../components/noise').then(({ Noise }) => Noise), {ssr: false});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Noise />
+      <Component {...pageProps} />
+    </>
+  );
 }
